4abca4f1336b1829db2b0126e56d5b18f18583a5
Helio Franca <helio.franca@rsinet.com.br>
2019-07-25 18:13:25 -0300

    Modulo ES6

---
+++ JavaScript Intermediário/Variavel/animal.js
@@ -0,0 +1,20 @@
+class Animal {
+
+    constructor(tipo, pernas) {
+        this.tipo = tipo;
+        this.perna = pernas;
+    }
+
+    falar(som = 'Som qualquer') {
+        return som;
+    }
+
+    get dados() {
+        return 'tipo: $(this.tipo), Pernas: ${this.pernas}';
+    }
+
+}
+
+export class Gato extends Animal{
+
+}
---
+++ JavaScript Intermediário/Variavel/matematica.js
@@ -0,0 +1,11 @@
+export function somar(a, b) {
+    return a + b;
+}
+
+function subtrair(a, b) {
+    return a + b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
