d0370e0eb83727224c383a433e9053c5175908be
Helio Franca <helio.franca@rsinet.com.br>
2019-08-05 20:07:44 -0300

    Personalização do TabNavigator

--- ReactNativeIntermediario/TerceiroProjeto/App.js
+++ ReactNativeIntermediario/TerceiroProjeto/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import { createAppContainer,createStackNavigator, Text } from 'react-navigation';
+import { View, Text, StyleSheet, Image, Button } from 'react-native';
+import { createBottomTabNavigator,createStackNavigator,createAppContainer,   } from 'react-navigation';
 
-import Feed from './src/screens/Feed';
-import Perfil from './src/screens/Perfil';
 
-const AppNavigator = createStackNavigator({
-  Feed:{
-    screen:Feed
-  },
-  Perfil:{
-    screen:Perfil
-  }},{
-      initialRouteName:'Feed',
-      headerMode:'float', //screen|float|none
-      headerBackTitleVisible:true,
-      headerLayoutPreset:'center',
-      defaultNavigationOptions:{
-        headerBackImage:()=>(
-          <Text>Voltar</Text>
+class Tela1 extends Component {
+    static navigationOptions = {
+        tabBarLabel: 'Tela1',
+        tabBarIcon: () => (
+            <Image source={require('./assets/images/house.png')} style={styles.icon} />
         )
-      }
-  });
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Esta é a tela 01</Text>
+            </View>
+        );
+    }
+}
 
-  export default createAppContainer(AppNavigator);
+class Tela2 extends Component {
+    static navigationOptions = {
+        tabBarLabel: 'Tela2',
+        tabBarIcon: () => (
+            <Image source={require('./assets/images/settings.png')} style={styles.icon} />
+        )
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Esta é a tela 02</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:20
+    },
+    icon:{
+        width:26,
+        height:26
+    }
+});
+
+const Navegador = createBottomTabNavigator({
+    Tela1:{
+        screen:Tela1
+    },
+    Tela2:{
+        screen:Tela2
+    }
+},
+
+{
+    tabBarPosition:'top',
+    animationEnabled:true,
+    initialRouteName:'Tela2',
+    order:['Tela2','Tela1'],
+    tabBarOptions:{
+        showIcon:true,
+        showLabel:true,
+        activeTintColor:'#FF0000',
+        inactiveTintColor:'#00FF00',
+        upperCaseLabel:false,
+        tabStyle:{
+            backgroundColor:'blue'
+        }
+     
+
+    }
+
+});
+
+const AppContainer = createAppContainer(Navegador);
+export default AppContainer;
